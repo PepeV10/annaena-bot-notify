6df97819a656ed75dcc0242e157ea19062ecc50c
PepeV10 <jo.ve.or.84@gmail.com>
2024-08-09 13:34:12 -0600

    Add annaena_bot_notify.py script

---
+++ annaena_bot_notify.py
@@ -0,0 +1,26 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
+
+# Enable logging
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi! You are now connected to the notification bot.')
+
+def help_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token("7098919168:AAF_n7g1D49w1Lbs0_xBpSgwFmUrVf3Mmu8").build()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("help", help_command))
+
+    app.run_polling()

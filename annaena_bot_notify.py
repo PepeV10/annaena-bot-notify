de50e57b600bbd5559d859fd04df7e9cf529a299
PepeV10 <jo.ve.or.84@gmail.com>
2024-08-09 18:07:50 -0600

    Fixed NameError by adding CallbackContext import

--- annaena_bot_notify.py
+++ annaena_bot_notify.py
@@ -1,11 +1,10 @@
 import logging
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler
+from telegram.ext import ApplicationBuilder, CommandHandler, CallbackContext
 
 # Enable logging
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
 )
 
 logger = logging.getLogger(__name__)
